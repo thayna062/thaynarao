e26d134fe14c23239575656737a4bab75d3d87c3
thayna062 <tavaresthaynara447@gmail.com>
2026-02-25 10:34:49 -0300

    Add HTML file to calculate cosine of a number

---
+++ ex06.js
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title> Este método retornará o cosseno (em radianos) de um número.</title>
+</head>
+autor:Thaynara
+
+<body>
+  <script>
+    var valor1 = Math.cos (0.14);
+    alert (valor1);
+  </script>
+</body>
+</html>
