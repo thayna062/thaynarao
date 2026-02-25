839c478970218e6252afac0652f6fc0175e06d6e
thayna062 <tavaresthaynara447@gmail.com>
2026-02-25 10:39:43 -0300

    Create ex09.js to compute natural logarithm
    Add HTML file that calculates natural logarithm of 2.2

---
+++ ex09.js
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>Este método retorna o logaritmo natural de um número (base E).</title>
+</head>
+autor:Thaynara
+
+<body>
+  <script>
+    var valor1 = Math.log (2.2);
+    alert (valor1);
+  </script>
+</body>
+</html>
