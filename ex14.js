fe09d589f5a9aee0b5d9f5e1f43c5e9ab8bdc206
thayna062 <tavaresthaynara447@gmail.com>
2026-02-25 10:48:55 -0300

    Add HTML file with rounding example using JavaScript

---
+++ ex14.js
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>Com este método é possível arredondar um valor.</title>
+</head>
+autor:Thaynara
+
+<body>
+  <script>
+    var valor1 = Math.round (121.6);
+    alert (valor1);
+  </script>
+</body>
+</html>
