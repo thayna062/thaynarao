99fa5b020627660a39cce651272af90972994697
thayna062 <tavaresthaynara447@gmail.com>
2026-02-25 10:25:33 -0300

    Add HTML file to calculate arccosine of a number

---
+++ ex02.js
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>Este método retornará o arco cosseno (em radianos) de um número.</title>
+</head>
+autor:Thaynara
+
+<body>
+  <script>
+    var valor1 = Math.acos (0.14);
+    alert (valor1);
+  </script>
+</body>
+</html>
