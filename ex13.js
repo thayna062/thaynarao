5ca8e75d5c28cda134b82c1c9fe08f669af2730a
thayna062 <tavaresthaynara447@gmail.com>
2026-02-25 10:47:15 -0300

    Add HTML file with random number alert

---
+++ ex13.js
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title> Este método retorna um número aleatório entre 0 e 1 com até 15 dígitos.</title>
+</head>
+autor:Thaynara
+
+<body>
+  <script>
+    alert (Math.random ());
+  </script>
+</body>
+</html>
