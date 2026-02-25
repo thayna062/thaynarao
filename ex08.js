eb35172e7216bcc35b84d18de85f4b1249c3ca57
thayna062 <tavaresthaynara447@gmail.com>
2026-02-25 10:38:35 -0300

    Create ex08.js to show Math.floor behavior
    Add HTML file to demonstrate Math.floor usage

---
+++ ex08.js
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title>Este método retorna o maior inteiro menor ou igual a um número.</title>
+</head>
+autor:Thaynara
+
+<body>
+  <script>
+    var valor1 = Math.floor (100.25);
+    var valor2 = Math.floor (-100.25);
+    alert (valor1);
+    alert (valor2);
+  </script>
+</body>
+</html>
