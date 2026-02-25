4c1f414e9d42050fc6fa1a5250bae8fecdff7bcb
thayna062 <tavaresthaynara447@gmail.com>
2026-02-25 10:49:56 -0300

    Add HTML file to calculate sine of a number

---
+++ ex15.js
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title> Este método retorna o seno de um número.</title>
+</head>
+autor:Thaynara
+
+<body>
+  <script>
+    var valor1 = Math.sin (1.4);
+    alert (valor1);
+  </script>
+</body>
+</html>
