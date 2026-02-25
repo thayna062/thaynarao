da97668f9f99d68a1bb410133b69e7b3eef7ee20
thayna062 <tavaresthaynara447@gmail.com>
2026-02-25 10:14:22 -0300

    Add HTML program to calculate absolute value

---
+++ ex01.js
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>progama que retorna o valor absoluto de um numero</title>
+autor:Thaynara
+
+<body>
+  <script>
+    var valor1 = Math.abs (-1234);
+    alert (valor1);
+  </script>
+</body>
+</html>
