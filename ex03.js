8d0131e6844dfd63a4a920b4a324348bd1e8dc93
thayna062 <tavaresthaynara447@gmail.com>
2026-02-25 10:26:42 -0300

    Add HTML file to calculate arcsine of a value

---
+++ ex03.js
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>Este método retorna o arco seno (em radianos) de um valor.</title>
+</head>
+autor:Thaynara
+
+<body>
+  <script>
+    var valor1 = Math.asin (0.14);
+    alert (valor1);
+  </script>
+</body>
+</html>
