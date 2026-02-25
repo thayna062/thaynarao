4722c28e9eb7debb53e66698cc68915e88a94106
thayna062 <tavaresthaynara447@gmail.com>
2026-02-25 10:54:21 -0300

    Create ex17.js

---
+++ ex17.js
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>Retorna a tangente de um número, que é equivalente a divisão do seno pelo cosseno deste mesmo valor.</title>
+</head>
+autor:Thaynara
+
+<body>
+  <script>
+    var valor1 = Math.tan (1.5);
+    alert (valor1);
+  </script>
+</body>
+</html>
